8ed6cc0e1fbcdd09fa46dc9c577dd1f26734a091
caicre <31694206+caicre@users.noreply.github.com>
2017-09-07 20:20:13 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/cai/newsapp/MainActivity.java
+++ app/src/main/java/com/example/cai/newsapp/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.cai.newsapp;
 
+import NewsApi.NewsSearchType;
+import NewsApi.NewsThread;
+import Interface.CallBack;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,12 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Console console = new Console();
+        NewsThread runnable = new NewsThread(console, NewsSearchType.Latest, 1, 10, NewsThread.science);
+        Thread thread = new Thread(runnable);
+        thread.start();
+
         newsTitleList = (TextView) findViewById(R.id.news_title_list);
-        newsTitleList.append("新闻标题列表\n\n");
+        newsTitleList.append(console.str);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);          //打开页面时调用，将对应的xml文件与menu类绑定
         return true;
+    }
+}
+
+class Console implements CallBack {
+    public String str;
+
+    public Console() {
+        str = new String("news!");
+    }
+
+    public void CallBackStr(String str){
+        this.str = str;
     }
 }
