2bf53b35622f1688f6d8d254f30280f45fbf4666
LunaElf <flameyeelf@gmail.com>
2017-09-07 19:47:20 +0800

    wyh

--- app/src/main/java/com/example/cai/newsapp/MainActivity.java
+++ app/src/main/java/com/example/cai/newsapp/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.cai.newsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView newsTitleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newsTitleList = (TextView) findViewById(R.id.news_title_list);
+        newsTitleList.append("新闻标题列表\n\n");
     }
 }
