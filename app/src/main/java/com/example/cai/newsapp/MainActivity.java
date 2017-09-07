bb48281e49e4597b3a93b5140a8248fccf759f44
LunaElf <flameyeelf@gmail.com>
2017-09-07 20:46:28 +0800

    1

--- app/src/main/java/com/example/cai/newsapp/MainActivity.java
+++ app/src/main/java/com/example/cai/newsapp/MainActivity.java
@@ -4,6 +4,9 @@ import NewsApi.NewsSearchType;
 import NewsApi.NewsThread;
 import Interface.CallBack;
 
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,12 +15,15 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView newsTitleList;
+    private SwipeRefreshLayout swipeRefresh;
+    private String key="0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SwipeRefresh();
         Console console = new Console();
         NewsThread runnable = new NewsThread(console, NewsSearchType.Latest, 1, 10, NewsThread.science);
         Thread thread = new Thread(runnable);
@@ -25,6 +31,36 @@ public class MainActivity extends AppCompatActivity {
 
         newsTitleList = (TextView) findViewById(R.id.news_title_list);
         newsTitleList.append(console.str);
+    }
+    public void SwipeRefresh(){
+        newsTitleList = (TextView) findViewById(R.id.news_title_list);
+
+        swipeRefresh = (SwipeRefreshLayout)findViewById(R.id.swipeLayout);
+        swipeRefresh.setColorSchemeResources(R.color.blue);
+        swipeRefresh.setSize(SwipeRefreshLayout.DEFAULT);
+        swipeRefresh.setProgressBackgroundColor(R.color.colorPrimary);
+        swipeRefresh.setProgressViewEndTarget(true, 200);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
+            @Override
+            public void onRefresh(){
+                Console console = new Console();
+                NewsThread runnable = new NewsThread(console, NewsSearchType.Latest, 1, 10, NewsThread.science);
+                Thread thread = new Thread(runnable);
+                thread.start();
+                try{
+                    Thread.sleep(1500);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                newsTitleList = (TextView) findViewById(R.id.news_title_list);
+                newsTitleList.clearComposingText();
+                newsTitleList.append(console.str);
+                swipeRefresh.setRefreshing(false);
+                //mHandler.sendEmptyMessage(1);
+
+
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
@@ -42,5 +78,6 @@ class Console implements CallBack {
 
     public void CallBackStr(String str){
         this.str = str;
+
     }
 }
