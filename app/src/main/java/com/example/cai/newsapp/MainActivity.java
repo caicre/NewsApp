2e1eb934363ef9d39036da614a8df752766a260f
LunaElf <flameyeelf@gmail.com>
2017-09-07 19:58:08 +0800

    wyh

--- app/src/main/java/com/example/cai/newsapp/MainActivity.java
+++ app/src/main/java/com/example/cai/newsapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cai.newsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         newsTitleList = (TextView) findViewById(R.id.news_title_list);
         newsTitleList.append("新闻标题列表\n\n");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);          //打开页面时调用，将对应的xml文件与menu类绑定
+        return true;
     }
 }
