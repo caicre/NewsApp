42f22f8f09b07b8acf8e2e45173e10cfe6223d61
caicre <31694206+caicre@users.noreply.github.com>
2017-09-07 22:46:23 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/cai/newsapp/MainActivity.java
+++ app/src/main/java/com/example/cai/newsapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.cai.newsapp;
 
 import NewsApi.NewsSearchType;
 import NewsApi.NewsThread;
-import Interface.CallBack;
+import Console.Console;
 
 import android.os.Handler;
 import android.os.Message;
@@ -17,21 +17,22 @@ public class MainActivity extends AppCompatActivity {
     private TextView newsTitleList;
     private SwipeRefreshLayout swipeRefresh;
     private String key="0";
+    private Console console;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        newsTitleList = (TextView) findViewById(R.id.news_title_list);
         SwipeRefresh();
-        Console console = new Console();
+        console = new Console(newsTitleList);
         NewsThread runnable = new NewsThread(console, NewsSearchType.Latest, 1, 10, NewsThread.science);
         Thread thread = new Thread(runnable);
         thread.start();
 
-        newsTitleList = (TextView) findViewById(R.id.news_title_list);
         newsTitleList.append(console.str);
-    }
+
     public void SwipeRefresh(){
         newsTitleList = (TextView) findViewById(R.id.news_title_list);
 
@@ -43,22 +44,11 @@ public class MainActivity extends AppCompatActivity {
         swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
             @Override
             public void onRefresh(){
-                Console console = new Console();
-                NewsThread runnable = new NewsThread(console, NewsSearchType.Latest, 1, 10, NewsThread.science);
-                Thread thread = new Thread(runnable);
-                thread.start();
-                try{
-                    Thread.sleep(1500);
-                }catch(InterruptedException e){
-                    e.printStackTrace();
-                }
+
                 newsTitleList = (TextView) findViewById(R.id.news_title_list);
                 newsTitleList.setText("");
                 newsTitleList.append(console.str);
                 swipeRefresh.setRefreshing(false);
-                //mHandler.sendEmptyMessage(1);
-
-
             }
         });
     }
@@ -66,18 +56,5 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);          //打开页面时调用，将对应的xml文件与menu类绑定
         return true;
-    }
-}
-
-class Console implements CallBack {
-    public String str;
-
-    public Console() {
-        str = new String("news!");
-    }
-
-    public void CallBackStr(String str){
-        this.str = str;
-
     }
 }
