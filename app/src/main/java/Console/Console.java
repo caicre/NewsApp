d679307551131f4595dafb1d52ced0b709f87adf
caicre <31694206+caicre@users.noreply.github.com>
2017-09-08 14:05:57 +0800

    增加获取新闻图片的功能

--- app/src/main/java/Console/Console.java
+++ app/src/main/java/Console/Console.java
@@ -3,19 +3,23 @@ package Console;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.TextView;
+import android.graphics.Bitmap;
 
 /**
  * Created by cai on 2017/9/7.
  */
 
 public class Console {
+    public static final int refresh = 1;
+
     public String str;
     public TextView newsTitleList;
 
     public Handler handler = new Handler() {
         public void handleMessage(Message msg) {
             switch(msg.what) {
-                case 1:
+                case refresh:
+                    //更新ui
                     newsTitleList.setText(str);
                     newsTitleList.invalidate();
                     break;
@@ -30,5 +34,8 @@ public class Console {
 
     public void CallBackStr(String str){
         this.str = str;
+    }
+
+    public void CallBackBitmap(Bitmap bitmap, String ID) {
     }
 }
