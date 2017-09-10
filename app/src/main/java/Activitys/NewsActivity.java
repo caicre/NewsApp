b254f2f06279b00d93a9764963b09851abfaf888
yyom11 <yyom11@naver.com>
2017-09-11 01:33:44 +0800

    lrx: 不用的文件删除

--- app/src/main/java/Activitys/NewsActivity.java
+++
@@ -1,9 +0,0 @@
-package Activitys;
-
-/**
- * Created by cai on 2017/9/8.
- */
-
-public interface NewsActivity {
-        public void refresh();
-}
--- app/src/main/java/NewsApi/NewsSearchType.java
+++
@@ -1,9 +0,0 @@
-package NewsApi;
-
-/**
- * Created by cai on 2017/9/7.
- */
-
-public enum NewsSearchType {
-    Latest, Keyword, Id, Picture
-}
